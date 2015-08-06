07bbf6957574f5788ed0324dff44fc9df307fa03
viktoria-a <vianlo@Viktorias-MacBook-Pro.local>
2015-08-06 14:59:35 +0200

    bytt namn på abortratt1 till question

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -3,6 +3,6 @@ from quiz import views
 urlpatterns = [
 	url("^$", views.surveys),
 	url(r"^quiz/([a-z-]+)/$", views.index),
-	url(r"^quiz/[a-z-]+/question/[0-9]/$", views.abortratt1),
+	url(r"^quiz/[a-z-]+/question/[0-9]/$", views.question),
 	url(r"^quiz/[a-z-]+/completed/$", views.resultat),
 	]
