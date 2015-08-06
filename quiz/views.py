76ddc4505d25b093fbad6b7452e43cba436de4b7
viktoria-a <vianlo@Viktorias-MacBook-Pro.local>
2015-08-06 10:31:04 +0200

    lagt till sökvägar i views till filerna

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def startpage(request):
+return render(request, “quiz/surveys.html”)
+def quiz(request):
+return render(request, “quiz/index.html”)
+def question(request):
+return render(request, “quiz/abortratt1.html”)
+def completed(request):
+return render(request, “quiz/resultat.html”)
+
