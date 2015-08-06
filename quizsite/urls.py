c7254269345daed6815468ad456181fb77233a18
viktoria-a <vianlo@Viktorias-MacBook-Pro.local>
2015-08-06 16:19:18 +0200

    flyttat parentes

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -3,6 +3,6 @@ from quiz import views
 urlpatterns = [
 	url("^$", views.surveys),
 	url(r"^quiz/([a-z-]+)/$", views.index),
-	url(r"^quiz/[a-z-]+/question/[0-9]/$", views.question),
-	url(r"^quiz/[a-z-]+/completed/$", views.resultat),
+	url(r"^quiz/([a-z-]+)/question/([0-9]+)/$", views.question),
+	url(r"^quiz/([a-z-]+)/completed/$", views.resultat),
 	]
