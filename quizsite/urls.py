6e7bb73c8c2e3706af84a7ef7860d09d96e7937f
viktoria-a <vianlo@Viktorias-MacBook-Pro.local>
2015-08-06 10:37:51 +0200

    bytt namn på url

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -19,3 +19,12 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+from django.conf.urls import url
+from quiz import views
+urlpatterns = [
+    url(“^$”, views.surveys),
+    url(r"^quiz/[a-z-]+/$", views.index),
+    url(r"^quiz/[a-z-]+/question/[0-9]/$", views.abortratt1),
+    url(r"^quiz/[a-z-]+/completed/$", views.resultat),
+]
