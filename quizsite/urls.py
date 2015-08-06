c50b39ff3f2ca54780795a7bbf5c7e5c45dff0eb
viktoria-a <vianlo@Viktorias-MacBook-Pro.local>
2015-08-06 10:38:47 +0200

    tagit bort deafulttext

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -13,12 +13,6 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-]
 
 from django.conf.urls import url
 from quiz import views
