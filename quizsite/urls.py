53d7e4ea9be29dc1db168f8b9ecfa14044658c97
viktoria-a <vianlo@Viktorias-MacBook-Pro.local>
2015-08-06 11:13:51 +0200

    tagit bort utf8

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -1,9 +1,7 @@
-#!/usr/bin/python
-# -*- coding: <utf-8> -*-
 from django.conf.urls import url
 from quiz import views
 urlpatterns = [
-    url(“^$”, views.surveys),
+    url("^$", views.surveys),
     url(r"^quiz/[a-z-]+/$", views.index),
     url(r"^quiz/[a-z-]+/question/[0-9]/$", views.abortratt1),
     url(r"^quiz/[a-z-]+/completed/$", views.resultat),
