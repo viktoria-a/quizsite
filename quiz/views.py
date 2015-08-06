64b5bdf181d5cc401ce9097d890a9dbccac2bbd6
viktoria-a <vianlo@Viktorias-MacBook-Pro.local>
2015-08-06 11:14:22 +0200

    def views efter mina filer

--- quiz/views.py
+++ quiz/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-def startpage(request):
-return render(request, “quiz/surveys.html”)
-def quiz(request):
-return render(request, “quiz/index.html”)
-def question(request):
-return render(request, “quiz/abortratt1.html”)
-def completed(request):
-return render(request, “quiz/resultat.html”)
+def surveys(request):
+	return render(request, "quiz/surveys.html")
+
+def index(request):
+	return render(request, "quiz/index.html")
+
+def abortratt1(request):
+	return render(request, "quiz/abortratt1.html")
+
+def resultat(request):
+	return render(request, "quiz/resultat.html")
 
