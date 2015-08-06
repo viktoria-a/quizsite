fe3107e35a3375ba31930ab8da59a9a9fdbe781d
viktoria-a <vianlo@Viktorias-MacBook-Pro.local>
2015-08-06 19:30:25 +0200

    lagt till model question

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Quiz(models.Model):
+	name = models.CharField(max_length=100)
+	slug = models.SlugField(max_length=100)
+	description = models.TextField()
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, related_name="questions")
+    question = models.TextField()
+    answer1 = models.CharField(max_length=100)
+    answer2 = models.CharField(max_length=100)
+    answer3 = models.CharField(max_length=100)
+    correct = models.PositiveIntegerField()
