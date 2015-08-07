f88e8f1f26eb7793da328868396bc8f1d331e5ff
viktoria-a <vianlo@Viktorias-MacBook-Pro.local>
2015-08-07 15:41:42 +0200

    uppdaterat models

--- quiz/models.py
+++ quiz/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Quiz(models.Model):
-	name = models.CharField(max_length=100)
-	slug = models.SlugField(max_length=100)
+	name = models.CharField(max_length=300)
+	slug = models.SlugField(max_length=300)
 	description = models.TextField()
 	def __unicode__ (self):
 		return self.name
@@ -12,9 +12,9 @@ class Quiz(models.Model):
 class Question(models.Model):
     quiz = models.ForeignKey(Quiz, related_name="questions")
     question = models.TextField()
-    answer1 = models.CharField(max_length=100)
-    answer2 = models.CharField(max_length=100)
-    answer3 = models.CharField(max_length=100)
+    answer1 = models.CharField(max_length=300)
+    answer2 = models.CharField(max_length=300)
+    answer3 = models.CharField(max_length=300)
     correct = models.PositiveIntegerField()
     def __unicode__(self):
     	return self.quiz.name + " / " + self.question
