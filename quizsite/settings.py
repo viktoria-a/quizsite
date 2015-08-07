e65983a18b25d638cc0a0b3d4a38181ec9a815a8
viktoria-a <vianlo@Viktorias-MacBook-Pro.local>
2015-08-07 13:34:10 +0200

    ändrat database

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -76,17 +76,6 @@ WSGI_APPLICATION = 'quizsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "static"),
-# )
-
 import dj_database_url
 DATABASES = {
     'default': dj_database_url.config(
@@ -94,6 +83,11 @@ DATABASES = {
 'db.sqlite3'))
     )
 }
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 
 # Internationalization
