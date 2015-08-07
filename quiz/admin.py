05ac3c032a7e3875f34a15586d6deb69e68e7973
viktoria-a <vianlo@Viktorias-MacBook-Pro.local>
2015-08-07 09:33:30 +0200

    models

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from quiz.models import Quiz, Question
+admin.site.register(Quiz)
+admin.site.register(Question)
 
 # Register your models here.
--- quiz/models.py
+++ quiz/models.py
@@ -6,6 +6,8 @@ class Quiz(models.Model):
 	name = models.CharField(max_length=100)
 	slug = models.SlugField(max_length=100)
 	description = models.TextField()
+	def __unicode__ (self):
+		return self.name
 
 class Question(models.Model):
     quiz = models.ForeignKey(Quiz, related_name="questions")
@@ -14,3 +16,5 @@ class Question(models.Model):
     answer2 = models.CharField(max_length=100)
     answer3 = models.CharField(max_length=100)
     correct = models.PositiveIntegerField()
+    def __unicode__(self):
+    	return self.quiz.name + " / " + self.question
