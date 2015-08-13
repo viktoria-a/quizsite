ca2a5ec275d88079fa36551602be7355279af9b8
viktoria-a <viktoria.asp@gmail.com>
2015-08-13 19:29:15 +0200

    ändrat till första startsidan

--- quizsite/urls.py
+++ quizsite/urls.py
@@ -5,7 +5,7 @@ from quiz import views
 urlpatterns = [
 	url("^$", views.quizzes, name="quizzes_page"),
 	url(r"^quiz/([a-z-]+)/$", views.quiz, name="quiz_page"),
-	url(r"^quiz/([a-z-]+)/question/([0-9]+)/$", views.question, name="question_page"),
+	url(r"^quiz/(slug=[a-z-]+)/question/(number=[0-9]+)/$", views.question, name="question_page"),
 	url(r"^quiz/([a-z-]+)/completed/$", views.resultat, name="resultat_page"),
 	url(r'^admin/', include(admin.site.urls)),
 	]
